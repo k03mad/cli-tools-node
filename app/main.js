54a4cbbe8c36036787a3daf40eae9b6b693c6bc4
k03mad <k03.mad@gmail.com>
2021-01-29 16:47:10 +0300

    add update notify

--- app/main.js
+++ app/main.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
+
 'use strict';
 
 const asTable = require('as-table');
+const pkg = require('../package.json');
 const replaceVersion = require('./lib/replace');
+const updateNotifier = require('update-notifier');
+
+updateNotifier({pkg}).notify();
 
 replaceVersion()
     .then(res => console.log(asTable(res)))
